4aec72d0ce34b055d4084964fb0f3bb9a061fc24
wengmu <dunkin9905@gmail.com>
2025-08-22 03:00:10 +0800

    Implement Level-2 Add, List

--- src/main/java/Chia.java
+++ src/main/java/Chia.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Chia {
     public static void main(String[] args) {
@@ -12,17 +14,27 @@ public class Chia {
         System.out.println("What can I help you with? \n");
 
         Scanner sc = new Scanner(System.in);
+        List<String> work_list = new ArrayList<>();
 
         while(true) {
             String user_input = sc.nextLine().trim();
             if (user_input.equals("bye")){
                 System.out.println("Goodbye . Hope you have a great day ahead!\n");
                 break;
-            } else if(user_input.equals("sing")) {
+            } else if(user_input.equals("list")) {
+                if (work_list.isEmpty()) {
+                    System.out.println("There is no work to do. Congrats!");
+                } else {
+                    for(int i = 0; i < work_list.size(); i++) {
+                        System.out.printf("%d. %s\n", i+1, work_list.get(i));
+                    }
+                }
+            }else if(user_input.equals("sing")) {
                 System.out.println("I get money, I'm a star\n" +
                         "Star, star, star, star, star, star");
-            } else {
-                System.out.println(user_input);
+            } else if(!user_input.isEmpty()){
+                work_list.add(user_input);
+                System.out.println("added: " + user_input);
             }
         }
     }
