4f8b3a2f93d06c15b3479c0ddaf6490118728ffb
wengmu <dunkin9905@gmail.com>
2025-08-22 01:52:04 +0800

    Implement Level-0 add greetings

---
+++ src/main/java/Chia.java
@@ -0,0 +1,13 @@
+public class Chia {
+    public static void main(String[] args) {
+        String logo =
+                          "   ____ _     _       \n"
+                        + "  / ___| |__ (_) __ _ \n"
+                        + " | |   | '_ \\| |/ _` |\n"
+                        + " | |___| | | | | (_| |\n"
+                        + "  \\____|_| |_|_|\\__,_|\n";
+        System.out.println("Hello Sir, I am \n" + logo);
+        System.out.println("What can I help you with?\n");
+        System.out.println("Goodbye. Hope you have a great day ahead!\n");
+    }
+}
