69150074ae671311dea8d6187a8d5fbf6955658d
wengmu <dunkin9905@gmail.com>
2025-08-22 02:36:40 +0800

    Implement Level-1 Echo

--- src/main/java/Chia.java
+++ src/main/java/Chia.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Chia {
     public static void main(String[] args) {
         String logo =
@@ -7,7 +9,21 @@ public class Chia {
                         + " | |___| | | | | (_| |\n"
                         + "  \\____|_| |_|_|\\__,_|\n";
         System.out.println("Hello Sir, I am \n" + logo);
-        System.out.println("What can I help you with?\n");
-        System.out.println("Goodbye. Hope you have a great day ahead!\n");
+        System.out.println("What can I help you with? \n");
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            String user_input = sc.nextLine().trim();
+            if (user_input.equals("bye")){
+                System.out.println("Goodbye . Hope you have a great day ahead!\n");
+                break;
+            } else if(user_input.equals("sing")) {
+                System.out.println("I get money, I'm a star\n" +
+                        "Star, star, star, star, star, star");
+            } else {
+                System.out.println(user_input);
+            }
+        }
     }
 }
