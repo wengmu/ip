2c7f92d3dba7d40d134724a2960fb537f0dd8eae
FlyingMun <wengmunchia@gmail.com>
2025-08-29 23:56:07 +0800

    Implement Level-8: Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,36 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task{
-    private final String by;
+    private LocalDate by;
+    private String originalInput;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            this.by = LocalDate.parse(by);
+            this.originalInput = by;
+        } catch(DateTimeParseException e) {
+            this.by = null;
+            this.originalInput = by;
+        }
     }
 
     public String getBy() {
-        return by;
+        if (by != null) {
+            return by.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+        } else {
+            return originalInput;
+        }
+    }
+
+    public String getByFile() {
+        return originalInput;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + " )";
+        return "[D]" + super.toString() + " (by: " + getBy() + " )";
     }
 }
