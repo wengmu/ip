4e3a96a2243bb5eee82739bd8ab811d4ee62655c
FlyingMun <wengmunchia@gmail.com>
2025-08-30 21:33:05 +0800

    Add A-JavaDoc: Comprehensive documentation

--- src/main/java/chia/task/Deadline.java
+++ src/main/java/chia/task/Deadline.java
@@ -4,10 +4,17 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+/**
+ To represents a deadline task - a task that needs to be done by a specific date.
+ */
 public class Deadline extends Task{
     private LocalDate by;
     private String originalInput;
 
+    /**
+     Creates a new deadline task with the given description and deadline
+     Attempts to parse the deadline as a LocalDate, falls back to storing as string if the parsing were to fail
+     */
     public Deadline(String description, String by) {
         super(description);
         try {
@@ -19,6 +26,9 @@ public class Deadline extends Task{
         }
     }
 
+    /**
+     Gets the deadline in a user-friendly format.
+     */
     public String getBy() {
         if (by != null) {
             return by.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
@@ -27,6 +37,9 @@ public class Deadline extends Task{
         }
     }
 
+    /**
+     Gets the original deadline input for file storage purposes
+     */
     public String getByFile() {
         return originalInput;
     }
--- src/main/java/chia/task/Task.java
+++ src/main/java/chia/task/Task.java
@@ -1,26 +1,47 @@
 package chia.task;
 
+/*
+ Use for representing a task with a description and completion status.
+ Each task has a description and can be marked as done or not done.
+ */
+
 public class Task {
     private final String description;
     private boolean isDone;
 
+    /**
+     Creates a new task with the given description.
+     The task is initially marked as not done.
+     */
     public Task(String description){
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     Marks the task as completed.
+     */
     public void mark(){
         this.isDone = true;
     }
 
+    /**
+     Marks the task as incompleted.
+     */
     public void unmark(){
         this.isDone = false;
     }
 
+    /**
+     Checks whether this task is completed.
+     */
     public boolean isDone() {
         return isDone;
     }
 
+    /**
+     Gets the description of this task.
+     */
     public String getDescription() {
         return description;
     }
